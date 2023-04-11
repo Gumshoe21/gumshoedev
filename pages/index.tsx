33211543bd9ca01501a50ec82547f7c0595621e0
Matthew Smilansky <msmilansky@gmail.com>
2023-04-11 13:04:34 -0700

    FEAT: Smoother scroll progress bar on Hero.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,22 @@ import Hero from '@home/Sections/Hero/Hero'
 import AboutMe from '@home/Sections/AboutMe'
 import Technologies from '@home/Sections/Technologies/Technologies'
 import Projects from '@home/Sections/Projects/Projects'
-import { motion, useScroll } from 'framer-motion'
+import { motion, useScroll, useSpring } from 'framer-motion'
 
 const Home = () => {
   const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  })
   return (
     <>
       <Navbar />
 
       <Box sx={{ 'scroll-behavior': 'smooth' }}>
         <Flex scrollBehavior='smooth' direction='column' align='center' justify='center' maxW='6xl' mx='auto' px='20'>
-          <motion.div className='progress-bar' style={{ scaleX: scrollYProgress }} />
+          <motion.div className='progress-bar' style={{ scaleX }} />
           <Hero />
           <AboutMe />
           <Technologies />
