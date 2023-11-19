64f18b5f649e552d410d3e8f5e803056b4730f1e
Matthew Smilansky <msmilansky@gmail.com>
2023-11-19 12:42:55 -0800

    feat: add Twitch bot to projects

--- components/Home/Sections/Projects/Projects.tsx
+++ components/Home/Sections/Projects/Projects.tsx
@@ -1,101 +1,142 @@
-import { Flex, Box, Text, Heading, useColorModeValue } from '@chakra-ui/react'
-import NextLink from 'next/link'
-import placeholder from '@public/placeholder300x250.png'
-import Image from '@components/Image'
-import Project from '@projects/Project'
-import SectionHeading from '@home/Sections/SectionHeading'
-import thumbnailLetterGo from '@public/app-lettergo.png'
-import thumbnail12HourStudy from '@public/app-12hourstudy.png'
-import thumbnailTheGrind from '@public/app-thegrind.png'
-import SectionLayout from '@components/Layouts/SectionLayout';
-const Projects = () => {
-  const heroBgColor = useColorModeValue('whiteAlpha.900', 'gray.800')
+import { Flex, Box, Text, Heading, useColorModeValue } from "@chakra-ui/react";
+import NextLink from "next/link";
+import placeholder from "@public/placeholder300x250.png";
+import Image from "@components/Image";
+import Project from "@projects/Project";
+import SectionHeading from "@home/Sections/SectionHeading";
+import thumbnailLetterGo from "@public/app-lettergo.png";
+import thumbnail12HourStudy from "@public/app-12hourstudy.png";
+import thumbnailTwitch from "@public/twitch.png";
+import thumbnailTheGrind from "@public/app-thegrind.png";
+import SectionLayout from "@components/Layouts/SectionLayout";
 
-  return (
+const Projects = () => {
+	const heroBgColor = useColorModeValue("whiteAlpha.900", "gray.800");
+
+	return (
 		<SectionLayout>
-			<Flex id='projects' direction='column' py={32}>
-				<Flex direction='column' gap={24} bg={heroBgColor} transitionDuration='200ms'>
-					<Flex direction='column' align='center' justify='center'>
+			<Flex id="projects" direction="column" py={32}>
+				<Flex
+					direction="column"
+					gap={24}
+					bg={heroBgColor}
+					transitionDuration="200ms"
+				>
+					<Flex direction="column" align="center" justify="center">
 						<SectionHeading>Projects</SectionHeading>
 					</Flex>
 					{/* Begin projects */}
-					<Flex direction='column' align='center' justify='center' gap='4.8rem'>
+					<Flex direction="column" align="center" justify="center" gap="4.8rem">
 						{/* Project 1 */}
 						<Project
-							githubHref={'https://github.com/Gumshoe21/12-Hour-Study'}
-							title='12 Hour Study'
-							href='https://12hourstudy.netlify.app'
+							githubHref={"https://github.com/Gumshoe21/12-Hour-Study"}
+							title="12 Hour Study"
+							href="https://12hourstudy.netlify.app"
 							thumbnailSrc={thumbnail12HourStudy}
-							linkText='Live Demo'
-							type='odd'
+							linkText="Live Demo"
+							type="odd"
 						>
 							<Text>
-								A&nbsp;<strong>Pomodoro timer app</strong>&nbsp; aimed at boosting productivity. The app generates
-								reports based on your study habits, and gives you an idea of how much studying you&apos;re doing over
-								time. The app was inspired by the &quot;study with me&quot; trend of videos on YouTube, which often
-								feature an on-screen Pomodoro timer.
+								A&nbsp;<strong>Pomodoro timer app</strong>&nbsp; aimed at
+								boosting productivity. The app generates reports based on your
+								study habits, and gives you an idea of how much studying
+								you&apos;re doing over time. The app was inspired by the
+								&quot;study with me&quot; trend of videos on YouTube, which
+								often feature an on-screen Pomodoro timer.
 							</Text>
 							<Text
-								as='i'
-								fontWeight='500'
-								textAlign={{ base: 'center', md: 'left' }}
-								fontSize='md'
-								textDecoration='italic'
+								as="i"
+								fontWeight="500"
+								textAlign={{ base: "center", md: "left" }}
+								fontSize="md"
+								textDecoration="italic"
 							>
-								Built with Next.js, Express.js, MongoDB, React.js, Nivo, and Chakra UI.
+								Built with Next.js, Express.js, MongoDB, React.js, Nivo, and
+								Chakra UI.
 							</Text>
 						</Project>
 						{/* End Project 1 */}
 						{/* Project 2 */}
 						<Project
-							githubHref={'https://github.com/Gumshoe21/lettergo'}
-							title='LetterGo'
-							href='https://lettergo.vercel.app'
+							githubHref={"https://github.com/Gumshoe21/lettergo"}
+							title="LetterGo"
+							href="https://lettergo.vercel.app"
 							thumbnailSrc={thumbnailLetterGo}
-							linkText='Live Demo'
-							type='even'
+							linkText="Live Demo"
+							type="even"
 						>
 							<Text>
-								A&nbsp;<strong>word game</strong>&nbsp;that challenges users to form as many heterograms as possible
-								before running out of time. Allows for OAuth 2 login via Google and GitHub. Features a leaderboard
-								listing the highest scores.
+								A&nbsp;<strong>word game</strong>&nbsp;that challenges users to
+								form as many heterograms as possible before running out of time.
+								Allows for OAuth 2 login via Google and GitHub. Features a
+								leaderboard listing the highest scores.
 							</Text>
-							<Text as='i' fontWeight='500' textAlign={{ base: 'center', md: 'right' }} fontSize='md'>
-								Built with Next.js, NextAuth, Prisma, PostgreSQL, and Tailwind CSS/UI.
+							<Text
+								as="i"
+								fontWeight="500"
+								textAlign={{ base: "center", md: "right" }}
+								fontSize="md"
+							>
+								Built with Next.js, NextAuth, Prisma, PostgreSQL, and Tailwind
+								CSS/UI.
 							</Text>
 						</Project>
 						{/* End project 2 */}
 						{/* Project 3 */}
 
 						<Project
-							githubHref={'https://github.com/Gumshoe21/thegrind'}
-							title='The Grind'
-							href='https://thegrind.vercel.app'
+							githubHref={"https://github.com/Gumshoe21/thegrind"}
+							title="The Grind"
+							href="https://thegrind.vercel.app"
 							thumbnailSrc={thumbnailTheGrind}
-							linkText='Live Demo'
-							type='odd'
+							linkText="Live Demo"
+							type="odd"
 						>
 							<Text>
-								A&nbsp;<strong>mock website for a fictional coffee shop.</strong>&nbsp; Features an online ordering
-								system with e-commerce components.
+								A&nbsp;
+								<strong>mock website for a fictional coffee shop.</strong>&nbsp;
+								Features an online ordering system with e-commerce components.
 							</Text>
 							<Text
-								as='i'
-								fontWeight='500'
-								textAlign={{ base: 'center', md: 'left' }}
-								fontSize='md'
-								textDecoration='italic'
+								as="i"
+								fontWeight="500"
+								textAlign={{ base: "center", md: "left" }}
+								fontSize="md"
+								textDecoration="italic"
 							>
 								Built with Next.js, NextAuth, MongoDB, and Tailwind CSS/UI.
 							</Text>
 						</Project>
 						{/* End Project 3*/}
+						{/* Project 4 */}
+						<Project
+							githubHref={"https://github.com/Gumshoe21/FatYoshiBot"}
+							title="Twitch Bot"
+							href="https://github.com/Gumshoe21/FatYoshiBot"
+							thumbnailSrc={thumbnailTwitch}
+							type="even"
+						>
+							<Text>
+								A&nbsp;<strong>Twitch chat bot </strong>&nbsp;that features
+								custom commands and channel point redemption events.
+							</Text>
+							<Text
+								as="i"
+								fontWeight="500"
+								textAlign={{ base: "center", md: "right" }}
+								fontSize="md"
+							>
+								Built with TypeScript, MongoDB, Twitch API, and the helpful
+								feedback of my viewers.
+							</Text>
+						</Project>
+						{/* End project 4 */}
 					</Flex>
 					{/* End Projects */}
 				</Flex>
 			</Flex>
 		</SectionLayout>
 	);
-}
+};
 
-export default Projects
+export default Projects;
