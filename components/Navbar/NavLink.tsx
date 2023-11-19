3b69c190c2b8ffcffe36f258db34c3bd2968762a
Matthew Smilansky <msmilansky@gmail.com>
2023-11-19 11:59:50 -0800

    fix: fix hydration issue arising from lack of accompanying <a> within <a>

--- components/Navbar/NavLink.tsx
+++ components/Navbar/NavLink.tsx
@@ -1,18 +1,18 @@
-import NextLink from 'next/link';
-import { Link, Text, useColorModeValue } from '@chakra-ui/react';
+import NextLink from "next/link";
+import { Link, Text, useColorModeValue } from "@chakra-ui/react";
 
 const NavLink = (props) => {
-  const navLinkHoverColor = useColorModeValue('blue.500', 'purple.300');
+  const navLinkHoverColor = useColorModeValue("blue.500", "purple.300");
   return (
-    <NextLink href={props.href} passHref>
-      <Link
-        _hover={{
-          color: navLinkHoverColor
-        }}
-      >
-        <Text fontWeight="bold">{props.text}</Text>
-      </Link>
-    </NextLink>
+    <Link
+      fontWeight="bold"
+      as={NextLink}
+      _hover={{ color: navLinkHoverColor }}
+      href={props.href}
+      passHref
+    >
+      {props.text}
+    </Link>
   );
 };
 
