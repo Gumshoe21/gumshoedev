a14efd6cc75566bef4d9c21bd2e0e0bc6d6060f5
Matthew Smilansky <msmilansky@gmail.com>
2023-11-19 13:13:33 -0800

    fix: fix TS error

--- components/Home/Sections/Projects/Project.tsx
+++ components/Home/Sections/Projects/Project.tsx
@@ -1,41 +1,81 @@
-import { Flex, Box, Text, Heading, useColorModeValue } from '@chakra-ui/react'
-import ChakraLink from '@components/ChakraLink'
-import Image from '@components/Image'
-import NextImage from 'next/image'
-import type { StaticImageData } from 'next/image'
+import { Flex, Box, Text, Heading, useColorModeValue } from "@chakra-ui/react";
+import ChakraLink from "@components/ChakraLink";
+import Image from "@components/Image";
+import NextImage from "next/image";
+import type { StaticImageData } from "next/image";
 
 interface IProject {
-  type: string
-  title: string
-  linkText: string
-  href: string
-  githubHref:string
-  thumbnailSrc: StaticImageData
-  children: JSX.Element[]
-
+  type: string;
+  title: string;
+  linkText?: string;
+  href: string;
+  githubHref: string;
+  thumbnailSrc: StaticImageData;
+  children: JSX.Element[];
 }
 
 const Project = (props: IProject) => {
-  const { type, title, linkText, thumbnailSrc, children, href, githubHref, ...rest } = props
+  const {
+    type,
+    title,
+    linkText,
+    thumbnailSrc,
+    children,
+    href,
+    githubHref,
+    ...rest
+  } = props;
 
-  const projectShadow = useColorModeValue('0px 13px 37px -12px #000', '0')
+  const projectShadow = useColorModeValue("0px 13px 37px -12px #000", "0");
 
   return (
-    <Flex direction={{ base: 'column-reverse', md: `${type === 'even' ? 'row-reverse' : 'row'}` }} justify='center' align='center' gap={{ base: 8, md: 4 }}>
+    <Flex
+      direction={{
+        base: "column-reverse",
+        md: `${type === "even" ? "row-reverse" : "row"}`,
+      }}
+      justify="center"
+      align="center"
+      gap={{ base: 8, md: 4 }}
+    >
       {/* Project description */}
-      <Flex direction='column' justify='space-around' flexBasis='66.6%' gap='8'>
-        <Flex direction='column'>
+      <Flex direction="column" justify="space-around" flexBasis="66.6%" gap="8">
+        <Flex direction="column">
           {/* Title */}
-          <Heading fontSize='3xl' mb={6} alignSelf={{ base: 'center', md: `${type === 'even' ? 'end' : 'start'}` }}>
+          <Heading
+            fontSize="3xl"
+            mb={6}
+            alignSelf={{
+              base: "center",
+              md: `${type === "even" ? "end" : "start"}`,
+            }}
+          >
             {title}
           </Heading>
           {/* Children (Paragraph and build with) */}
-          <Flex direction='column' fontWeight='normal' fontSize='md' gap='4' textAlign={{ base: 'center', md: `${type === 'even' ? 'right' : 'left'}` }}>
+          <Flex
+            direction="column"
+            fontWeight="normal"
+            fontSize="md"
+            gap="4"
+            textAlign={{
+              base: "center",
+              md: `${type === "even" ? "right" : "left"}`,
+            }}
+          >
             {children}
           </Flex>
         </Flex>
         {/* Live Demo link */}
-        <Flex fontSize='md' fontWeight='bold' direction='column' textAlign={{ base: 'center', md: `${type === 'even' ? 'right' : 'left'}` }}>
+        <Flex
+          fontSize="md"
+          fontWeight="bold"
+          direction="column"
+          textAlign={{
+            base: "center",
+            md: `${type === "even" ? "right" : "left"}`,
+          }}
+        >
           <ChakraLink href={href} {...rest}>
             {linkText}
           </ChakraLink>
@@ -47,12 +87,18 @@ const Project = (props: IProject) => {
       {/* Project image */}
 
       <ChakraLink href={href}>
-        <Flex _hover={{ transform: 'translateY(-10px)' }} transitionDuration='200ms' boxShadow={projectShadow} justify='space-between' align='space-between'>
-          <Image src={thumbnailSrc} alt='' />
+        <Flex
+          _hover={{ transform: "translateY(-10px)" }}
+          transitionDuration="200ms"
+          boxShadow={projectShadow}
+          justify="space-between"
+          align="space-between"
+        >
+          <Image src={thumbnailSrc} alt="" />
         </Flex>
       </ChakraLink>
     </Flex>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
