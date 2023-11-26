a73d81dbcc8cf56b45c6a7d88e17852aa4f05e54
Matthew Smilansky <msmilansky@gmail.com>
2023-11-26 15:53:54 -0800

    fix: Fix broken Twitter profile link on Hero.

--- components/Home/Sections/Hero/Hero.tsx
+++ components/Home/Sections/Hero/Hero.tsx
@@ -1,72 +1,82 @@
-import { keyframes, useColorModeValue, Box, Flex, Heading, Link } from '@chakra-ui/react'
-import profilePic from '@public/headshot.jpg'
+import {
+	keyframes,
+	useColorModeValue,
+	Box,
+	Flex,
+	Heading,
+	Link,
+} from "@chakra-ui/react";
+import profilePic from "@public/headshot.jpg";
 
-import SocialButton from './SocialButton'
-import { FaTwitter, FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'
-import Image from '@components/Image'
-import SectionLayout from '@components/Layouts/SectionLayout';
+import SocialButton from "./SocialButton";
+import { FaTwitter, FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+import Image from "@components/Image";
+import SectionLayout from "@components/Layouts/SectionLayout";
 const Hero = () => {
-  const section1BgColor = useColorModeValue('orange.100', 'gray.600')
-  const profilePicShadow = useColorModeValue('0px 12px 27px -12px #000', '0px 12px 27px -10px #fff')
-  const heroBgColor = useColorModeValue('whiteAlpha.900', 'gray.800')
+	const section1BgColor = useColorModeValue("orange.100", "gray.600");
+	const profilePicShadow = useColorModeValue(
+		"0px 12px 27px -12px #000",
+		"0px 12px 27px -10px #fff",
+	);
+	const heroBgColor = useColorModeValue("whiteAlpha.900", "gray.800");
 
-  const fadeIn = (dir: string): string => {
-    return keyframes`100% { ${dir}:0; }`
-  }
+	const fadeIn = (dir: string): string => {
+		return keyframes`100% { ${dir}:0; }`;
+	};
 
-  return (
+	return (
 		<SectionLayout>
 			<Flex
-				id='hero'
-				align='center'
-				justify='center'
-				flexDirection='column'
-				height='100vh'
-				transitionDuration='200ms'
+				id="hero"
+				align="center"
+				justify="center"
+				flexDirection="column"
+				height="100vh"
+				transitionDuration="200ms"
 				pt={40}
 				pb={20}
-				textAlign='center'
-				position='relative'
+				textAlign="center"
+				position="relative"
 			>
 				<Box
-					alignSelf='center'
-					borderRadius='50%'
-					position='relative'
-					top='-100%'
+					alignSelf="center"
+					borderRadius="50%"
+					position="relative"
+					top="-100%"
 					sx={{
-						animation: `1000ms ${fadeIn('top')} forwards`,
+						animation: `1000ms ${fadeIn("top")} forwards`,
 					}}
 				>
 					<img
-						id='arlen'
-						src='headshot.jpg'
-						alt='Profile Picture for Matthew Smilansky on his personal website, gumshoe.dev'
+						id="arlen"
+						src="headshot.jpg"
+						alt="Profile Picture for Matthew Smilansky on his personal website, gumshoe.dev"
 					/>
 				</Box>
 				<Heading
-					as='h1'
-					fontSize={{ base: '3xl', md: '5xl' }}
-					fontWeight='600'
-					letterSpacing='1px'
+					as="h1"
+					fontSize={{ base: "3xl", md: "5xl" }}
+					fontWeight="600"
+					letterSpacing="1px"
 					py={2}
-					position='relative'
-					right='-100%'
+					position="relative"
+					right="-100%"
 					sx={{
-						animation: `1000ms ${fadeIn('right')} forwards`,
-						animationDelay: '200ms',
+						animation: `1000ms ${fadeIn("right")} forwards`,
+						animationDelay: "200ms",
 					}}
 				>
 					Matthew Smilansky
 				</Heading>
 				<Heading
-					fontSize={{ base: 'lg', md: 'xl' }}
-					fontFamily='Montserrat'
-					fontWeight='600'
-					position='relative'
-					left='-100%'
+					fontSize={{ base: "lg", md: "xl" }}
+					fontFamily="Montserrat"
+					fontWeight="600"
+					position="relative"
+					left="-100%"
 					sx={{
-						animation: `1000ms ${fadeIn('left')} forwards`,
-						'animation-delay': '200ms',
+						animation: `1000ms ${fadeIn("left")} forwards`,
+						"animation-delay": "200ms",
 					}}
 				>
 					Software Engineer
@@ -74,20 +84,32 @@ const Hero = () => {
 				<Flex
 					columnGap={2}
 					py={2}
-					bottom='-100%'
-					position='relative'
+					bottom="-100%"
+					position="relative"
 					sx={{
-						animation: `1s ${fadeIn('bottom')} forwards`,
+						animation: `1s ${fadeIn("bottom")} forwards`,
 					}}
 				>
-					<SocialButton icon={<FaEnvelope size={26} />} href='mailto:msmilansky@gmail.com' />
-					<SocialButton icon={<FaTwitter size={26} />} href='https://twitter.com/gumshoe_dev' />
-					<SocialButton icon={<FaLinkedin size={26} />} href='https://linkedin.com/in/matthew-smilansky' />
-					<SocialButton icon={<FaGithub size={26} />} href='https://github.com/Gumshoe21' />
+					<SocialButton
+						icon={<FaEnvelope size={26} />}
+						href="mailto:msmilansky@gmail.com"
+					/>
+					<SocialButton
+						icon={<FaTwitter size={26} />}
+						href="https://twitter.com/gumshoedev"
+					/>
+					<SocialButton
+						icon={<FaLinkedin size={26} />}
+						href="https://linkedin.com/in/matthew-smilansky"
+					/>
+					<SocialButton
+						icon={<FaGithub size={26} />}
+						href="https://github.com/Gumshoe21"
+					/>
 				</Flex>
 			</Flex>
 		</SectionLayout>
 	);
-}
+};
 
-export default Hero
+export default Hero;
