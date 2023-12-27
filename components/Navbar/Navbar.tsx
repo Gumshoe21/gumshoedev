985f223c0fdf7b7e896a971dfa26bd3606b91772
Matthew Smilansky <msmilansky@gmail.com>
2023-12-27 10:53:50 -0800

    feat: Add resume links to Hero and Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -37,33 +37,40 @@ const Navbar = () => {
   const iconColor = useColorModeValue('whiteAlpha.900', 'gray.900')
 
   return (
-    <Box zIndex='2' position='fixed' css={{ backdropFilter: 'blur(5px)' }} w='100%'>
-      <Flex opacity='100%' py={4} alignItems='center' justify={{ md: 'center', sm: 'center' }} transitionDuration='200ms'>
-        <Flex w='100%' justify='center' align='center' flexDirection='row' columnGap={10}>
-          <NavLink href='#hero' text={'Home'} />
-          <NavLink href='#about' text={'About Me'} />
+		<Box zIndex='2' position='fixed' css={{ backdropFilter: 'blur(5px)' }} w='100%'>
+			<Flex
+				opacity='100%'
+				py={4}
+				alignItems='center'
+				justify={{ md: 'center', sm: 'center' }}
+				transitionDuration='200ms'
+			>
+				<Flex w='100%' justify='center' align='center' flexDirection='row' columnGap={10}>
+					<NavLink href='#hero' text={'Home'} />
+					<NavLink href='#about' text={'About Me'} />
 
-          <NavLink href='#techstack' text={'Tech Stack'} />
-          <NavLink href='#projects' text={'Projects'} />
-          <NavLink href='https://blog.gumshoe.dev' text={'Blog'} />
-          <IconButton
-            aria-label={' '}
-            bg={iconBackgroundColor}
-            color={iconColor}
-            variant='solid'
-            _hover={{ bg: iconBackgroundHover }}
-            icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-            onClick={toggleColorMode}
-          />
-        </Flex>
-      </Flex>
-      {isOpen ? (
-        <Box pb={4} display={{ md: 'none' }}>
-          <Stack as={'nav'} spacing={4} textAlign='center'></Stack>
-        </Box>
-      ) : null}
-    </Box>
-  )
+					<NavLink href='/resume.pdf' text={'Resume'} />
+					<NavLink href='#projects' text={'Projects'} />
+					<NavLink href='#techstack' text={'Tech Stack'} />
+					<NavLink href='https://blog.gumshoe.dev' text={'Blog'} />
+					<IconButton
+						aria-label={' '}
+						bg={iconBackgroundColor}
+						color={iconColor}
+						variant='solid'
+						_hover={{ bg: iconBackgroundHover }}
+						icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+						onClick={toggleColorMode}
+					/>
+				</Flex>
+			</Flex>
+			{isOpen ? (
+				<Box pb={4} display={{ md: 'none' }}>
+					<Stack as={'nav'} spacing={4} textAlign='center'></Stack>
+				</Box>
+			) : null}
+		</Box>
+	);
 }
 
 export default Navbar
