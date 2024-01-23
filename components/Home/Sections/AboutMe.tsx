86bd8e6632b3e697450bf9ac991b24897f1ea0b5
Matthew Smilansky <msmilansky@gmail.com>
2024-01-23 13:46:29 -0800

    fix: Change 'degree' to 'Bachelor of Arts degree' in About Me section.

--- components/Home/Sections/AboutMe.tsx
+++ components/Home/Sections/AboutMe.tsx
@@ -17,7 +17,7 @@ const AboutMe = () => {
 					</Flex>
 					<Flex fontSize='md' px={{ md: 20 }} direction={{ md: "row", base: "column" }} align='center' justify='space-between' gap={{ base: 8, md: 20 }}>
 						<Flex gap={4} textAlign='center' direction='column' fontSize='lg'>
-							<Text>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I was born in Los Angeles&#44; California and graduated from the University of California, Los Angeles with a degree in English Literature.</Text>
+							<Text>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I was born in Los Angeles&#44; California and graduated from the University of California, Los Angeles with a Bachelor of Arts degree in English Literature.</Text>
 							<Text>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I&apos;ve always had an interest in technology. When I was 4 years old&#44; my parents purchased a computer running Microsoft Windows 3.1 PC&#44; and I was immediately hooked.</Text>
 							<Text>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;After graduating from UCLA&#44; I started developing an interest in computer programming and began learning how to code websites and web applications. And here I am now!</Text>
 						</Flex>
